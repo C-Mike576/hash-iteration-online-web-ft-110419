3670d9d4cccfb9e840c757abbbd6aa2154cf3acf
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 14:29:39 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ def happy_birthday(birthday_kids)
 end
 
 
-age_appropriate_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid_name, age|
     if age <= 12
       puts "Happy Birthday #{kid_name}! You are now #{age} years old!"
